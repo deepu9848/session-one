9b05d9105db5908f0d213182618ff2a392f3f4b5
deepu9848 <awasti.deepak2013@gmail.com>
2023-04-11 23:01:16 -0400

    basic of oop

---
+++ src/ClassOne.java
@@ -0,0 +1,19 @@
+public class ClassOne {
+    /* status
+    ->Brand
+    ->Size
+    ->Shelves
+    ->Price
+    ->Capacity
+    ->Number of Doors
+    ->Door Material Type
+
+
+     */
+}/*constructions
+
+/*behaviours
+
+
+
+
